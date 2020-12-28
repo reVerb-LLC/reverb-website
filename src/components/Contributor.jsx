c4d74e25cf06f63e1d7603caf3f83f887d21f355
Corey Van Splinter <cvanspl1@gmail.com>
2020-12-28 09:49:45 -0500

    Added contributor component

---
+++ src/components/Contributor.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "../styles/Contributor.css";
+import githubLogo from "../media/externalLogos/github-logo.svg";
+import linkedInLogo from "../media/externalLogos/linkedin.svg";
+
+class Contributor extends Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.image} className="photo" alt="Sean"></img>
+        <p className="identifier">{this.props.name}</p>
+        <div className="logo-links">
+          <a href={this.props.github} target="_blank" rel="noreferrer">
+            <img src={githubLogo} className="logos" alt="GitHub"></img>
+          </a>
+          <a href={this.props.linkedIn} target="_blank" rel="noreferrer">
+            <img src={linkedInLogo} className="logos" alt="LinkedIn"></img>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contributor;
