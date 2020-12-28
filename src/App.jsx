4893c7c969e02a0297dd7cb76d769d43d1b54dbf
Corey Van Splinter <cvanspl1@gmail.com>
2020-12-28 09:50:54 -0500

    Added app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import About from "./components/About";
+import Team from "./containers/Team";
 import "./styles/App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Welcome />
+      <About />
+      <Team />
+      <div className="footer" />
+    </div>
+  );
 }
 
 export default App;
