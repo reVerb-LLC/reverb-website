27d76f976bd1cdb33982f09e22dc840e76dfd541
Corey Van Splinter <cvanspl1@gmail.com>
2021-01-04 20:58:37 -0500

    minor bug fix

--- src/components/Contributor.jsx
+++ src/components/Contributor.jsx
@@ -3,16 +3,16 @@ import "../styles/Contributor.css";
 import githubLogo from "../media/externalLogos/github-logo.svg";
 import linkedInLogo from "../media/externalLogos/linkedin.svg";
 
-function Contributor() {
+function Contributor({image, name, github, linkedIn}) {
   return (
     <div>
-      <img src={this.props.image} className="photo" alt="Sean"></img>
-      <p className="identifier">{this.props.name}</p>
+      <img src={image} className="photo" alt="Sean"></img>
+      <p className="identifier">{name}</p>
       <div className="logo-links">
-        <a href={this.props.github} target="_blank" rel="noreferrer">
+        <a href={github} target="_blank" rel="noreferrer">
           <img src={githubLogo} className="logos" alt="GitHub"></img>
         </a>
-        <a href={this.props.linkedIn} target="_blank" rel="noreferrer">
+        <a href={linkedIn} target="_blank" rel="noreferrer">
           <img src={linkedInLogo} className="logos" alt="LinkedIn"></img>
         </a>
       </div>
