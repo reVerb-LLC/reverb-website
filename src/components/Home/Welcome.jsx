c4faa2319d8ea024167c9b892bcb17e8f9a76eb5
Sean <itsmesean@pm.me>
2021-01-06 19:22:00 -0800

    Changed 'video demo' to link to docs

--- src/components/Home/Welcome.jsx
+++ src/components/Home/Welcome.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import reVerbLogo from "../../media/reverbLogos/reVerbLogo.svg";
 
-
 function Welcome() {
-  return (
-    <div id="welcome">
-      <img src={reVerbLogo} id="welcome-logo" alt="logo" />
-      <div id="details">
-        <p id="welcome-description">A RESTful API client for Visual Studio Code</p>
-        <div className="welcome-buttons">
-          <button>Video Demo</button>
-          <a
-            href="https://marketplace.visualstudio.com/items?itemName=reverb-api.reverb"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button>Install reVerb</button>
-          </a>
+    return (
+        <div id="welcome">
+            <img src={reVerbLogo} id="welcome-logo" alt="logo" />
+            <div id="details">
+                <p id="welcome-description">
+                    A RESTful API client for Visual Studio Code
+                </p>
+                <div className="welcome-buttons">
+                    <a
+                        href="https://reverb-llc.github.io/reverb-docs/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <button>Documentation</button>
+                    </a>
+                    <a
+                        href="https://marketplace.visualstudio.com/items?itemName=reverb-api.reverb"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <button>Install reVerb</button>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Welcome;
